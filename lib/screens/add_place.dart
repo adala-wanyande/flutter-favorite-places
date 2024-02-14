ab849658c41b921c7106396e5cfbe3f39f03bccd
adala-wanyande <benardwanyande@gmail.com>
2024-02-14 11:22:58 +0300

    chore: add location package

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
-import 'package:favorite_places/widgets/Image_input.dart';
+import 'package:favorite_places/widgets/image_input.dart';
+import 'package:favorite_places/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -63,6 +64,10 @@ class _AddPlacesScreenState extends ConsumerState<AddPlacesScreen> {
             ImageInput(onPickImage: (image) {
               _selectedImage = image;
             }),
+            const SizedBox(
+              height: 10,
+            ),
+            const LocationInput(),
             const SizedBox(
               height: 16,
             ),
