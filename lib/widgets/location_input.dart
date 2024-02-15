e9f9b9007bb1665313cd232cc1a67ae2dc15a4bc
adala-wanyande <benardwanyande@gmail.com>
2024-02-15 11:01:09 +0300

    feat: display location preview map snapshot using Google Maps API

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -19,6 +19,15 @@ class _LocationInputState extends State<LocationInput> {
   PlaceLocation? _pickedLocation;
   var _isGettingLocation = false;
 
+  String get locationImage {
+    if (_pickedLocation == null) {
+      return '';
+    }
+    final lat = _pickedLocation!.latitude;
+    final lng = _pickedLocation!.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyC_qBAhT5XAzk2667NZR29A73I8nMUIOfs';
+  }
+
   void _getCurrentLocation() async {
     Location location = Location();
     bool serviceEnabled;
@@ -77,6 +86,15 @@ class _LocationInputState extends State<LocationInput> {
             color: Theme.of(context).colorScheme.onBackground,
           ),
     );
+
+    if (_pickedLocation != null) {
+      previewContent = Image.network(
+        locationImage,
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      );
+    }
 
     if (_isGettingLocation) {
       previewContent = const CircularProgressIndicator();
